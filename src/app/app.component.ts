2cf7259733117cec50f52f28331222799a1a5985
Alberto Basalo <albertobasalo@hotmail.com>
2018-08-14 18:53:39 +0200

    minor change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   template: `
     <header class="hero">
       <div class="hero-body has-text-centered">
-        <h1 class="title"> Bienvenido a {{title}} </h1>
-        <h2 class="subtitle"> Versión: {{subtitle}} </h2>
+        <h1 class="title"> Welcome to {{title}} </h1>
+        <h2 class="subtitle"> Version: {{subtitle}} </h2>
         <a target="_blank" rel="noopener" href="https://academia-binaria.com/">
           <img width="100" src="./assets/logo.png">
         </a>
@@ -34,9 +34,7 @@ import { Component } from '@angular/core';
           <strong>Autobot.</strong><i>-A simple Angular project for playing with cars... while learning Angular ;-)</i>.
         </p>
         <p>
-          <small>
-          By <a href="https://twitter.com/albertobasalo">Alberto Basalo</a>
-          </small>
+          <small>By <a href="https://twitter.com/albertobasalo">Alberto Basalo</a></small>
         </p>
       </div>
     </footer>
