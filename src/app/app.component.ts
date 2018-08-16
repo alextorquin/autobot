097d34f20253b83499aedd2007314f54271ffddd
Alberto Basalo <albertobasalo@hotmail.com>
2018-08-16 13:32:29 +0200

    1-base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,46 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <header class="hero">
-      <div class="hero-body has-text-centered">
-        <h1 class="title"> Welcome to {{title}} </h1>
-        <h2 class="subtitle"> Version: {{subtitle}} </h2>
-        <a target="_blank" rel="noopener" href="https://academia-binaria.com/">
-          <img width="100" src="./assets/logo.png">
-        </a>
-      </div>
-    </header>
-    <main class="section">
-      <div class="container">
-        <h2 class="title">Links to learn Angular: </h2>
-        <ul>
-          <li>
-            <h2><a target="_blank" rel="noopener" href="https://academia-binaria.com/hola-angular-cli/">Tutorial en español</a></h2>
-          </li>
-          <li>
-            <h2><a target="_blank" rel="noopener" href="https://github.com/AcademiaBinaria/autobot/tree/0-hello">GitHub Repository</a></h2>
-          </li>
-          <li>
-            <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-          </li>
-        </ul>
-      </div>
-    </main>
-    <footer>
-      <div class="content has-text-centered">
-        <p>
-          <strong>Autobot.</strong><i>-A simple Angular project for playing with cars... while learning Angular ;-)</i>.
-        </p>
-        <p>
-          <small>By <a href="https://twitter.com/albertobasalo">Alberto Basalo</a></small>
-        </p>
-      </div>
-    </footer>
-  `,
+  template: `<app-navigator></app-navigator> `,
   styles: []
 })
-export class AppComponent {
-  title = 'autobot';
-  subtitle = '0-hello';
-}
+export class AppComponent {}
