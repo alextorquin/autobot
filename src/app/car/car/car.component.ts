295a4dd60d68161f8301d22553b702ef568f029b
Alberto Basalo <albertobasalo@hotmail.com>
2018-08-27 20:26:47 +0200

    speed distance

--- src/app/car/car/car.component.ts
+++ src/app/car/car/car.component.ts
@@ -18,18 +18,20 @@ import { Car } from '../../core/store/models/car.model';
       <div class="content">
         <div>
           Current speed : {{ car.currentSpeed }} km/h of {{ car.maxSpeed }} km/h maximum
+          <progress class="progress is-info" [value]="car.currentSpeed" [max]="car.maxSpeed"></progress>
         </div>
         <div>
           Traveled : {{ car.distanceTraveled }} km of {{ car.maxDistance }} km maximum
+          <progress class="progress is-info" [value]="car.maxDistance - car.distanceTraveled" [max]="car.maxDistance"></progress>
         </div>
       </div>
     </div>
     <footer class="card-footer">
       <div class="card-footer-item">
-        <button class="button is-danger is-outlined">Brake</button>
+        <button class="button is-danger is-outlined" (click)="onBrake()">Brake</button>
       </div>
       <div class="card-footer-item">
-        <button class="button is-primary is-outlined">Throttle</button>
+        <button class="button is-primary is-outlined" (click)="onThrottle()">Throttle</button>
       </div>
     </footer>
   </div>
@@ -45,4 +47,36 @@ export class CarComponent implements OnInit {
     const carId = this.route.snapshot.params['carId'];
     this.car = Cars.find(c => c.link.url === carId);
   }
+
+  public onBrake() {
+    if (this.checkDistance()) {
+      if (this.car.currentSpeed > 0) {
+        this.car.currentSpeed--;
+      } else {
+        this.car.currentSpeed = 0;
+      }
+      this.updateDistance();
+    } else {
+      this.stopCar();
+    }
+  }
+
+  public onThrottle() {
+    if (this.checkDistance()) {
+      if (this.car.currentSpeed < this.car.maxSpeed) {
+        this.car.currentSpeed++;
+      } else {
+        this.car.currentSpeed = this.car.maxSpeed;
+      }
+      this.updateDistance();
+    } else {
+      this.stopCar();
+    }
+  }
+
+  private stopCar = () => (this.car.currentSpeed = 0);
+
+  private checkDistance = () => this.car.distanceTraveled < this.car.maxDistance;
+
+  private updateDistance = () => (this.car.distanceTraveled += this.car.currentSpeed);
 }
