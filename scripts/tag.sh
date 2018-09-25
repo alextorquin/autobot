6f70320f13f88c7058e7c10c279af57e3971db4b
Alberto Basalo <albertobasalo@hotmail.com>
2018-09-25 09:14:28 +0200

    tag 8

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,5 +1,5 @@
-git merge 7-watch
-git tag -a v7.0.0 -m "7-Vigilancia y seguridad en Angular"
+git merge 8-reactive
+git tag -a v8.0.0 -m "8-Formularios reactivos con Angular"
 git push origin master --tag
 
 # git push --delete origin v0.0.0
