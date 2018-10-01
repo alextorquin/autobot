11c737a80eb684a399107b9dc5efaf75d729bc9f
Alberto Basalo <albertobasalo@hotmail.com>
2018-10-01 13:25:45 +0200

    @ngrx/schematics @ngrx/store-devtools @ngrx/router-store

--- scripts/lib/ngrx.sh
+++ scripts/lib/ngrx.sh
@@ -1,2 +1,9 @@
-ng add @ngrx/store
-ng add @ngrx/effects
+# ng add @ngrx/store
+# ng add @ngrx/effects
+
+# npm install @ngrx/schematics --save-dev
+# ng config cli.defaultCollection @ngrx/schematics
+# npm install @ngrx/store-devtools @ngrx/router-store --save
+
+
+# https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=en
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { AppRoutingModule } from './app-routing.module';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { AppEffects } from './app.effects';
+import { CoreModule } from './core/core.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { metaReducers, reducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [AppRoutingModule, BrowserModule, CoreModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot([AppEffects])],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    CoreModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([AppEffects])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
