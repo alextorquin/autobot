ab8c134ad720bf31b0275b006d1091051658889a
Alberto Basalo <albertobasalo@hotmail.com>
2018-10-01 13:38:03 +0200

    ng rx base config router config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppEffects } from './app.effects';
 import { CoreModule } from './core/core.module';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { metaReducers, reducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    CoreModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([AppEffects])
-  ],
+  imports: [AppRoutingModule, BrowserModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,29 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
 import { AuthInterceptorService } from './auth-interceptor.service';
 import { FooterComponent } from './navigator/footer.component';
 import { HeaderComponent } from './navigator/header.component';
 import { MainComponent } from './navigator/main.component';
 import { NavigatorComponent } from './navigator/navigator.component';
+import { AppEffects } from './store/app.effects';
+import { metaReducers, reducers } from './store/reducers';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule, RouterModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    RouterModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects])
+  ],
   declarations: [NavigatorComponent, HeaderComponent, MainComponent, FooterComponent],
   exports: [NavigatorComponent],
   providers: [
