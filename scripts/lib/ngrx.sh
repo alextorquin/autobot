6e373f4a4eb802cab28856dd06c7bbf7f7130be4
Alberto Basalo <albertobasalo@hotmail.com>
2018-10-03 12:34:02 +0200

    ng g effect

--- scripts/lib/ngrx.sh
+++ scripts/lib/ngrx.sh
@@ -5,5 +5,6 @@
 # ng config cli.defaultCollection @ngrx/schematics
 # npm install @ngrx/store-devtools @ngrx/router-store --save
 ng generate store Global -m core/core.module.ts --statePath core/store/global/reducers
+ng g effect Cars --root --module=./../../../core.module.ts
 
 # https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=en
