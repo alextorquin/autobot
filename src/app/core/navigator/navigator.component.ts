1bacd7e319e44136b6684a30fc952105cb654c05
Alberto Basalo <albertobasalo@hotmail.com>
2018-10-10 18:05:05 +0200

    update?

--- src/app/core/navigator/navigator.component.ts
+++ src/app/core/navigator/navigator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
+import { UpdateAvailableEvent } from '@angular/service-worker/src/low_level';
 import { GlobalStoreService } from '../global-store.service';
 
 @Component({
@@ -15,12 +17,22 @@ export class NavigatorComponent implements OnInit {
   public userMessage$;
   constructor(
     private globalStore: GlobalStoreService,
-    private router: Router
+    private router: Router,
+    private swUpdate: SwUpdate
   ) {}
 
   ngOnInit() {
     this.globalStore.selectLoginNeeded$().subscribe(this.onLoginNeededChange);
     this.userMessage$ = this.globalStore.selectUserMessage$();
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe((event: UpdateAvailableEvent) => {
+        this.globalStore.dispatchUserMessage('There are available updates!!!');
+        const msg = `Do you want to update?`;
+        if (confirm(msg)) {
+          window.location.reload();
+        }
+      });
+    }
   }
 
   private onLoginNeededChange = (loginNeeded: boolean) => {
