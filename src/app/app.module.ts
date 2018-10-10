d86d97aff95d6f0c07f6d3785a6de02aaa7b3a95
Alberto Basalo <albertobasalo@hotmail.com>
2018-10-10 13:36:44 +0200

    ng add @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [AppRoutingModule, BrowserModule, CoreModule],
+  imports: [AppRoutingModule, BrowserModule, CoreModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
