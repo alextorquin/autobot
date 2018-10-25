dc44b4b6f073082da6846436055b92d81c4fc461
Alberto Basalo <albertobasalo@hotmail.com>
2018-10-25 12:06:13 +0200

    meta

--- src/app/core/navigator/navigator.component.ts
+++ src/app/core/navigator/navigator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 import { GlobalStoreService } from '../global-store.service';
 
 @Component({
@@ -14,12 +15,16 @@ import { GlobalStoreService } from '../global-store.service';
 })
 export class NavigatorComponent implements OnInit {
   public userMessage$;
-  constructor(private globalStore: GlobalStoreService, private router: Router, private title: Title) {}
+  constructor(private globalStore: GlobalStoreService, private router: Router, private title: Title, private meta: Meta) {}
 
   ngOnInit() {
+    this.meta.addTag({ property: 'og:title', content: environment.title }, true);
     this.globalStore.selectLoginNeeded$().subscribe(this.onLoginNeededChange);
     this.userMessage$ = this.globalStore.selectUserMessage$();
-    this.globalStore.selectTitle$().subscribe(title => this.title.setTitle(title));
+    this.globalStore.selectTitle$().subscribe(title => {
+      this.title.setTitle(title);
+      this.meta.updateTag({ property: 'og:title', content: title });
+    });
   }
 
   private onLoginNeededChange = (loginNeeded: boolean) => {
