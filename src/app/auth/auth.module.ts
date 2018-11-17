7eb57c7fdd2de56b8d20668cf46af77d2bbb7a55
Alberto Basalo <albertobasalo@hotmail.com>
2018-11-17 19:45:53 +0100

    pack:auth-blocks

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AuthBlocksModule } from 'AuthBlocks';
 import { SharedModule } from '../shared/shared.module';
 import { AccessComponent } from './access/access.component';
 import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  imports: [CommonModule, AuthRoutingModule, SharedModule],
+  imports: [CommonModule, AuthRoutingModule, SharedModule, AuthBlocksModule],
   declarations: [AccessComponent]
 })
 export class AuthModule {}
